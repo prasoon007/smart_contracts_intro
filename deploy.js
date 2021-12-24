97aa936289626f393a3bed2d9c6c0fa0bc772c24
prasoon007 <prasoonshukla9002@gmail.com>
2021-12-24 17:52:54 +0530

    initial commit

--- deploy.js
+++ deploy.js
@@ -12,7 +12,7 @@ const web3 = new Web3(provider);
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
 
-    console.log('Attemption to depoy from account', accounts[0]);
+    console.log('Attempt to depoy from account', accounts[0]);
 
     const result = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['Hi there!']})
